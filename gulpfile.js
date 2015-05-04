7ad9adf1fb214801d01b34c477c758cf8d6414ad
alu0100837353 <alu0100837353@ull.edu.es>
2015-05-04 20:27:10 +0000

    Created gulpfile and tests

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp    = require('gulp');
+
+gulp.task('test', function () {
+ 
+});
+
+/**gulp.task('default', function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
+**/
